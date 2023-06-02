d1b45786fdcda49a3f13433a4519a0dc81476505
tw2013.kim <tigger6392@gmail.com>
2023-06-02 12:55:46 +0900

    [feature/length] Imple & Tests added
    Signed-off-by: tw2013.kim <tigger6392@gmail.com>

--- Project1/similarityCheck.cpp
+++ Project1/similarityCheck.cpp
@@ -1,9 +1,75 @@
+#include <codecvt>
 #include <string>
 #include <vector>
+#include <iostream>
+#include <stdexcept>
 using namespace std;
+
+const int EXCEPTIONAL_CASES = -1;
+const int NORMAL_CASES = 0;
+const int MAX_LENGTH_SCORE = 60;
+
 
 class SimilarityCheck
 {
 public:
+    int LengthCheck(string input1, string input2)
+    {
+        if (CheckInputStrings(input1, input2) == EXCEPTIONAL_CASES)
+            return EXCEPTIONAL_CASES;
+        if (CheckDoubleStrings(input1, input2))	return 0;
 
+        return GetScoreForNormalCases(input1, input2);
+    }
+
+private:
+    int CheckInputStrings(string input1, string input2)
+    {
+        try
+        {
+            CheckInvalidInput(input1, input2);
+        }
+        catch (invalid_argument& e)
+        {
+            cout << "ERR: " << e.what() << endl;
+            return EXCEPTIONAL_CASES;
+        }
+        return NORMAL_CASES;
+    }
+    void CheckInvalidInput(const string& input1, const string& input2)
+    {
+        if (input1.length() == 0 || input2.length() == 0)
+        {
+            throw invalid_argument("Empty string!");
+        }
+        if (HasWrongChar(input1) || HasWrongChar(input2))
+        {
+            throw invalid_argument("Has wrong char!");
+        }
+    }
+    bool HasWrongChar(const string& input)
+    {
+        for (int i = 0; i < input.length(); i++)
+        {
+            if (!(input[i] >= 'A' && input[i] <= 'Z')) return true;
+        }
+        return false;
+    }
+    bool CheckDoubleStrings(const string& input1, const string& input2)
+    {
+        if (input1.length() >= (input2.length() * 2) ||
+            input2.length() >= (input1.length() * 2))
+        {
+            return true;
+        }
+        return false;
+    }
+    int GetScoreForNormalCases(string input1, string input2)
+    {
+        if (input1.length() == input2.length())	return MAX_LENGTH_SCORE;
+        int gap = max(input1.length(), input2.length()) -
+            min(input1.length(), input2.length());
+        return MAX_LENGTH_SCORE -
+            (MAX_LENGTH_SCORE * gap / min(input1.length(), input2.length()));
+    }
 };
--- Sample-Test1/similarityCheck_test.cpp
+++ Sample-Test1/similarityCheck_test.cpp
@@ -1,7 +1,47 @@
 #include "pch.h"
 #include "../Project1/similarityCheck.cpp"
 
-TEST(SimilarityCheck, SimCheck) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+class SimCheckFixture : public testing::Test
+{
+public:
+    SimilarityCheck sc;
+};
+
+TEST_F(SimCheckFixture, Length_Except_No_String) {
+    EXPECT_EQ(EXCEPTIONAL_CASES, sc.LengthCheck("", ""));
+    EXPECT_EQ(EXCEPTIONAL_CASES, sc.LengthCheck("A", ""));
+    EXPECT_EQ(EXCEPTIONAL_CASES, sc.LengthCheck("", "A"));
+}
+
+TEST_F(SimCheckFixture, Length_Except_Lowercase) {
+    EXPECT_EQ(EXCEPTIONAL_CASES, sc.LengthCheck("a", "A"));
+    EXPECT_EQ(EXCEPTIONAL_CASES, sc.LengthCheck("A", "b"));
+}
+
+TEST_F(SimCheckFixture, Length_Except_Number) {
+    EXPECT_EQ(EXCEPTIONAL_CASES, sc.LengthCheck("1", "A"));
+    EXPECT_EQ(EXCEPTIONAL_CASES, sc.LengthCheck("A", "8"));
+}
+
+TEST_F(SimCheckFixture, Length_Special_Char) {
+    EXPECT_EQ(EXCEPTIONAL_CASES, sc.LengthCheck("-", "A"));
+    EXPECT_EQ(EXCEPTIONAL_CASES, sc.LengthCheck("A", "@"));
+}
+
+TEST_F(SimCheckFixture, Length_Double_String) {
+    EXPECT_EQ(0, sc.LengthCheck("ABC", "A"));
+    EXPECT_EQ(0, sc.LengthCheck("ABC", "ABCDEFG"));
+    EXPECT_EQ(0, sc.LengthCheck("A", "ABCDEF"));
+}
+
+TEST_F(SimCheckFixture, Length_Perfect_Cases) {
+    EXPECT_EQ(MAX_LENGTH_SCORE, sc.LengthCheck("ABC", "CDE"));
+    EXPECT_EQ(MAX_LENGTH_SCORE, sc.LengthCheck("ABC", "ABC"));
+    EXPECT_EQ(MAX_LENGTH_SCORE, sc.LengthCheck("A", "X"));
+}
+
+TEST_F(SimCheckFixture, Length_Diff_Length) {
+    EXPECT_EQ(40, sc.LengthCheck("ABCD", "CDE"));
+    EXPECT_EQ(20, sc.LengthCheck("ABCDE", "ABC"));
+    EXPECT_EQ(0, sc.LengthCheck("AB", "X"));
 }
