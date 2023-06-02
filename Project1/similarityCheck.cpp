6ba66f7967c0c43a14d4ff8bf935b749f4794f19
tw2013.kim <tigger6392@gmail.com>
2023-06-02 11:14:41 +0900

    First Commit

---
+++ Project1/similarityCheck.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class SimilarityCheck
+{
+public:
+
+};
---
+++ Sample-Test1/similarityCheck_test.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "../Project1/similarityCheck.cpp"
+
+TEST(SimilarityCheck, SimCheck) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
